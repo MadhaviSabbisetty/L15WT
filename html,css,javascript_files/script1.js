570c2f0bf26e0c1c61e7950d82e593d972a602c2
MadhaviSabbisetty <130948826+MadhaviSabbisetty@users.noreply.github.com>
2023-04-18 14:29:19 +0530

    Add files via upload

---
+++ html,css,javascript_files/script1.js
@@ -0,0 +1,27 @@
+const orderForm = document.getElementById('order-form');
+const appetizers = document.getElementById('appetizers');
+const drinks = document.getElementById('drinks');
+const desserts = document.getElementById('desserts');
+const submitBtn = document.getElementById('submit-btn');
+const total = document.getElementById('total');
+
+// Calculate the total price
+function calculateTotal() {
+  const appetizersPrice = parseFloat(appetizers.value);
+  const drinksPrice = parseFloat(drinks.value);
+  const dessertsPrice = parseFloat(desserts.value);
+
+  const totalPrice = appetizersPrice + drinksPrice + dessertsPrice;
+  total.innerText = `${totalPrice.toFixed(2)}`;
+}
+
+// Add event listener to the form
+orderForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  calculateTotal();
+});
+
+// Add event listeners to the select elements
+//appetizers.addEventListener('change', calculateTotal);
+//drinks.addEventListener('change', calculateTotal);
+//desserts.addEventListener('change', calculateTotal);
